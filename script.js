56632bed177d4c4dc279449cc1c4e337fe41252b
Chama317 <Michel_Sebastian@outlook.com>
2024-03-19 00:05:21 -0300

    Image added

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 const menuButton = document.querySelector(".menuButton");
-const sideBar  = document.querySelector(".sidebar");
+const sideBar = document.querySelector(".sidebar");
 const closeMenu = document.querySelector(".closeMenu");
 const menuLinks = document.querySelectorAll(".sidebar a[href^='#']");
 
 menuButton.addEventListener("click", (showSideBAR) => {
-  sideBar.style.display = "block"
-})
+  sideBar.style.display = "block";
+});
 
 closeMenu.addEventListener("click", (hideSideBar) => {
-  sideBar.style.display = "none"
-})
+  sideBar.style.display = "none";
+});
 
-menuLinks.forEach(menuLinks => {
-  menuLinks.addEventListener("click", function(){
-    sideBar.style.display = "none"
-  })
-})
-
-
-
-
-
-
-
+menuLinks.forEach((menuLinks) => {
+  menuLinks.addEventListener("click", function () {
+    sideBar.style.display = "none";
+  });
+});
