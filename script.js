6e4a2daae9b7b7b668273d659b7304a661ca4fbd
Michel-Camacho <Michel_Sebastian@outlook.com>
2024-04-10 21:19:21 -0300

    scroll-up button added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuButton = document.querySelector(".menuButton");
 const sideBar = document.querySelector(".sidebar");
 const closeMenu = document.querySelector(".closeMenu");
 const menuLinks = document.querySelectorAll(".sidebar a[href^='#']");
+const scrollBtn = document.getElementById("scrollBtn");
 
 menuButton.addEventListener("click", (showSideBAR) => {
   sideBar.style.display = "block";
@@ -15,4 +16,9 @@ menuLinks.forEach((menuLinks) => {
   menuLinks.addEventListener("click", function () {
     sideBar.style.display = "none";
   });
+});
+
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
